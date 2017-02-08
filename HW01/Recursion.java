6f0e26daca2f5d778a05a8bbcd639299362ea6b5
Peter Lee <dlee14@stuy.edu>
2017-02-07 23:48:20 -0500

    finished

--- HW01/Recursion.java
+++ HW01/Recursion.java
@@ -1,3 +1,32 @@
 public class Recursion {
-  
+
+  public static String name() {
+    return "Lee,DongJun";
+  }
+
+  public static double sqrt(double n) {
+    if (n < 0) {
+      throw new IllegalArgumentException("Wrong value");
+    }
+    return guessSqrt(n, 1);
+  }
+
+  public static double guessSqrt(double n, double guess) {
+    if (closeEnough(n, Math.pow(guess, 2))) {
+      return guess;
+    } else {
+      double betterGuess = (n / guess + guess) / 2;
+      return guessSqrt(n, betterGuess);
+    }
+  }
+
+  private static boolean closeEnough(double n1, double n2) {
+    return (Math.abs(n1 - n2) / Math.max(Math.abs(n1), Math.abs(n2)) <= Math.pow(10, -8));
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sqrt(152));
+    System.out.println(sqrt(144));
+    System.out.println(sqrt(1571248));
+  }
 }
