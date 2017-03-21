274f8e3881685e909dd462bff5abc869a979743e
Peter Lee <dlee14@stuy.edu>
2017-03-21 15:35:26 -0400

    done

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -36,10 +36,6 @@ public class Quick{
         greater--;
       }
     }
-    System.out.println(Arrays.toString(data));
-    // System.out.println("less: " + less);
-    // System.out.println("i: " + i);
-    // System.out.println("greater: " + greater);
     return less;
   }
 
@@ -48,8 +44,26 @@ public class Quick{
   }
 
   private static void quickH(int[] data, int start, int end) {
-    if (data.length != 1) {
-      quickH(data, start, part(data, start, end) - 1);
+    if (start < end) {
+      int index = (int)(Math.random() * (end - start + 1) + start);
+      int pivot = data[index];
+      int less = start, greater = end, i = start + 1;
+      swap(data, start, index);
+      while (i <= greater) {
+        if (data[i] < data[less]) {
+          swap(data, i, less);
+          i++;
+          less++;
+        } else if (data[i] == data[less]) {
+          i++;
+        } else {
+          swap(data, i, greater);
+          greater--;
+        }
+      }
+      System.out.println(Arrays.toString(data));
+      quickH(data, start, less - 1);
+      quickH(data, i, end);
     }
   }
 
@@ -60,35 +74,50 @@ public class Quick{
     data[swap1] = save;
   }
 
-  // public static boolean test() {
-  //   boolean result = true;
-  //   int size = 10, number = 0, index = 0, max = 100, min = -100;
-  //   int[] test, sorted;
-  //   for (int i = 0; i < 100; i++) {
-  //     test = new int[size];
-  //     sorted = new int[size];
-  //     for (int j = 0; j < size; j++) {
-  //       Random rand = new Random();
-  //       number = rand.nextInt(max + 1 - min) + min;
-  //       test[j] = number;
-  //       sorted[j] = number;
-  //     }
-  //     index = part(test, 0, size - 1);
-  //     Arrays.sort(sorted);
-  //     if (index == 0) {
-  //       System.out.println(Arrays.toString(test));
-  //       System.out.println(Arrays.toString(sorted));
-  //     }
-  //     if (sorted[index] != test[index]) {
-  //       result = false;
-  //     }
-  //   }
-  //   return result;
-  // }
+  public static boolean test() {
+    boolean result = true;
+    int size = 10, number = 0, index = 0, max = 10, min = -10, i;
+    int[] test, sorted;
+    for (i = 0; i < 100; i++) {
+      test = new int[size];
+      sorted = new int[size];
+      for (int j = 0; j < size; j++) {
+        Random rand = new Random();
+        number = rand.nextInt(max + 1 - min) + min;
+        test[j] = number;
+        sorted[j] = number;
+      }
+      index = part(test, 0, size - 1);
+      Arrays.sort(sorted);
+      if (sorted[index] != test[index]) {
+        result = false;
+      }
+      for (i = 0; i < index; i++) {
+        if (test[i] > test[index]) {
+          result = false;
+        }
+      }
+      for (i = index; i < test.length; i++) {
+        if (test[i] < test[index]) {
+          result = false;
+        }
+      }
+    }
+    return result;
+  }
 
   public static void main(String[] args) {
     int[] data = {5,8,3,6,4,7,0,9,1,2};
-    Quick.quickSort(data);
+    int size = 30, number = 0, max = 10, min = -10;
+    int[] test;
+    test = new int[size];
+    for (int j = 0; j < size; j++) {
+      Random rand = new Random();
+      number = rand.nextInt(max + 1 - min) + min;
+      test[j] = number;
+    }
+    Quick.quickSort(test);
+    // Quick.quickSort(data);
     // System.out.println(Quick.part(data,0,9));
   }
 }
