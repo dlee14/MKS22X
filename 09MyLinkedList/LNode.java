059cb5989723e8a346e7ce1c986e00bea1dc4148
Peter Lee <dlee14@stuy.edu>
2017-03-28 15:10:01 -0400

    started linked lists

---
+++ 09MyLinkedList/LNode.java
@@ -0,0 +1,9 @@
+public class LNode {
+  private int value;
+  private LNode next, prev;
+
+  public LNode(int value) {
+    this.value = value;
+  }
+
+}
---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,8 @@
+public class MyLinkedList extends LNode {
+  private int size;
+  private LNode start, end;
+
+  public MyLinkedList() {
+
+  }
+}
