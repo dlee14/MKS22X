bd35b51f17f68f1d6515548ab41b40b095206b1b
Peter Lee <dlee14@stuy.edu>
2017-04-23 23:14:27 -0400

    finished deque

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+public class MyDeque {
+  private String[] ary;
+  private int size;
+  private int front, back;
+
+  public MyDeque() {
+    ary = new String[10];
+    size = 0;
+    front = 0;
+    back = 0;
+  }
+
+  public void addFirst(String value) {
+    if (value == null) {
+      throw new NullPointerException();
+    }
+    if (size == ary.length) {
+      resize();
+    }
+    if (size != 0) {
+      if (front == 0) {
+        front = ary.length - 1;
+      } else {
+        front--;
+      }
+    }
+    ary[front] = value;
+    size++;
+  }
+
+  public void addLast(String value) {
+    if (value == null) {
+      throw new NullPointerException();
+    }
+    if (size == ary.length) {
+      resize();
+    }
+    if (size != 0) {
+      back = (back + 1) % ary.length;
+    }
+    ary[back] = value;
+    size++;
+  }
+
+  public String removeFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    String output = ary[front];
+    front = (front + 1) % ary.length;
+    size--;
+    return output;
+  }
+
+  public String removeLast() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    String output = ary[back];
+    if (back == 0) {
+      back = ary.length - 1;
+    } else {
+      back--;
+    }
+    size--;
+    return output;
+  }
+
+  public String getFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    return ary[front];
+  }
+
+  public String getLast() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    return ary[back];
+  }
+
+  private void resize() {
+    String[] biggerAry = new String[ary.length * 2];
+    int i = 0;
+    for (int j = front; j < front + size; j++) {
+      biggerAry[i] = ary[j % ary.length];
+      i++;
+    }
+    ary = biggerAry;
+    front = 0;
+    back = size - 1;
+  }
+
+  public String toString() {
+    String output = "[";
+    for (int i = front; i < front + size; i++) {
+      output += ary[i % ary.length] + ", ";
+    }
+
+    if (size != 0) {
+      output = output.substring(0, output.length() - 2);
+    }
+    return output + "]";
+  }
+}
